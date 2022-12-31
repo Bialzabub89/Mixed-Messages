a5adcb26fb568d6d1d9287b351260854fca72c8d
CKWheatley <ckwheatley15@gmail.com>
2022-12-31 15:57:57 +0000

    Code clean & CSS
    Added some simple css, cleaned up some code previously.

--- main/functions.js
+++ main/functions.js
@@ -1,15 +1,19 @@
 "use strict";
 
+function grab_id(arg){
+    return document.getElementById(arg)
+}
+
 // Message ID target
-let message = document.getElementById('message')
+let message = grab_id('message')
 
 
 // Assigning Variables to buttons ID within index.html
-const animalsButton = document.getElementById('animals')
-const gamesButton = document.getElementById('games')
-const carsButton = document.getElementById('cars')
-const programmingButton = document.getElementById('programming')
-const randomButton = document.getElementById('random')
+const animalsButton = grab_id('animals')
+const gamesButton = grab_id('games')
+const carsButton = grab_id('cars')
+const programmingButton = grab_id('programming')
+const randomButton = grab_id('random')
 
 // Animal Messages
 const animal = [
@@ -64,75 +68,29 @@ const prog = [
 ];
 
 // Array of the Above Arrays
-const random = [
-    "There are around 700 seperate programming languages! Amongst this list, some of the most popular languages are Javascript, Swift, Scala, Python, PHP, Go, Rust, Ruby, and C#, with millions of users utilizing them in both their careers and personal projects. However, new programming languages are constantly being created.",
-
-    "Recent studies show that around 70% of coding jobs have nothing to do with technology at all! That's right, you or your child could learn to program and apply this knowledge to topics completely separate from technology - like nature studies, geography research, and film and design.",
-
-    "The first programming language was called FORTRAN! It was developed by a team lead by Mr. John Backus, an American computer scientist. First appearing in 1964, FORTRAN is still in use today, predominantly helping computer scientists to conduct complex tests in areas such as numerical weather prediction, geophysics, crystallography, and computational chemistry.",
-
-    "NASA still uses programs from the 70s in their spacecraft! According to experts, NASA chooses not to write new code or design new programs as it would be extremely expensive to carry this out. Alongside this, implementing new programs requires a lot of testing to minimize the chance of a catastrophic failure happening. Instead, they choose to stick to older tech that has proven to be reliable, safe, and non-costly",
-
-    "There are 3 very different types of Hackers, one being malicious, the other benevolent, and the last somewhere in between the two! Black hats are the first type and specialize in breaking into computer networks with malicious intent. Often, they will attempt to steal passwords, credit cards, and other types of sensitive information, blackmailing internet users for money in the process. On the flip side, White hats primarily choose to use their skills for good, regularly being contracted by businesses to look for security holes in their online infrastructures. Grey hats are a mixture of both, helping people yet also commonly exploiting their web-based vulnerabilities when not paid for their services.",
-
-    "The world’s first automobile was developed by Carl Benz in 1885. The top speed of the motor car was 16 km per hour. It had a one cylinder four-stroke engine installed horizontally on a specifically designed chassis. It was known as the Benz Patent Motorwagen.",
-
-    "In 1908, the Model T developed by Henry Ford became the first car available to the masses. This was a low-cost car. It cost around $850 in 1908 and $260 in 1925 (the reduction in the cost of the car was mainly because of the invention of the assembly line).",
-
-    " There are an estimated 1.2 billion cars in the world today while there were only 500 million in 1986. According to a report released by a research house Bernstein, there will be a total of 2 billion cars on the planet by 2040.",
-
-    "In 2013, white was the world's most favorite car color. According to an Australian study, white vehicles are 10% less likely to be involved in an accident. Moreover, cars that have mainstream colors are prone to theft more than those that have less popular colors.",
-
-    "Almost three-quarters of the Rolls Royce cars ever produced are still on the road today.",
-
-    "Batman: Arkham Asylum was almost rythmic and in 2D! You got that right - the famous Dark Knight was almost going to see a 2D release in a rhythmic fashion. The developers and designers created Batman: Arkham Asylum as a rhythmic action game during its earliest production. The second prototype focused on creating it in a 2D fighting sequence. They dodged a bullet there, didn't they?",
-
-    "Super Mario Charachters got their names from Famous Musicians! If you were observant like few other players, you would notice the pattern in Iggy, Ludwig, and Lemmy's names. Yes, most of the Koopalings in Super Mario got their names from famous musicians. Also, the fact you may never suspect is that the fire-breathing Triceratops, Reznor, in Super Mario, got named after Trent Reznor from Nine Inch Nails.",
-
-    "E.T the Extraterestrial by Atari was the worst game ever! In 1982, Atari made a huge mistake by releasing an entry titled E.T the Extraterrestrial. The game was so bad that it's widely considered the worst video game of all time. Urban legend has it that the company had to bury unsold copies of the game in the desert. This 'myth' turned out to be true when a documentary crew dug them up. But that's a whole other story.",
-
-    "Assasins Creed was a Prince of Persia spin off! The famous Assassin's Creed series from Ubisoft was initially a spinoff of the Prince of Persia. Furthermore, during its early design and thought, the game developers initially referred to it as Prince of Persia: Assassins. Originally, the story centered on a female assassin tasked with protecting Jerusalem’s Prince. However, Ubisoft turned the offer down as the story didn’t focus more on the Prince.",
-
-    "Minecraft is the best selling game ever! Minecraft is currently the best-selling game ever. Its creator Marcus Alexej Persson (a.k.a Notch), recorded over $180 million in sales. It also remains the most played game across all platforms.",
-
-    "Only half a Dolphin's brain sleeps at a time! Dolphins are mammals so although they live in the sea, and can hold their breath for an exceptionally long time, they do have to come up for air from time to time. It is for this reason that a Dolphin's brain will never fully disengage, as it still needs to retain enough motor control to occasionally go up to the surface and breath. Dolphins also sleep with one eye open at all times.",
-
-    "Female Ferrets die if they dont mate when they go into heat! When a female Ferret goes in to heat their bodies produce a high amount of the hormone 'oestrogen' which will continue to be produced en masse until she has mated. Having such high quantities of oestrogen for a prolonged period of will eventually result in Aplastic Anaemia, which is caused by the damage sustained by the bone marrow and blood cells by the high levels of the hormone.",
-
-    "Armadillos can catch Leprosy! Leprosy is a bacterial infection that affects humans and if left untreated will go on to cause severe damage to the skin, nerves, extremities and eyes; eventually leading to them going numb and allowing them to get secondary infections which may result in the loss of the appendage. Very few infections affect multiple species of animal, but leprosy is one of the few that actually can as it is able to infect both humans and, for some reason, Armadillos.",
-
-    "A Cockroaches brain is in its body! One of the ultimate survivors, the Cockroach, unlike most animals, can survive decapitation as its brain is not actually housed inside it, but rather inside of its body. Once decapitated a Cockroach will keep on ticking, walking around, going about its business; eventually dying from starvation as it no longer has a mouth to eat with.",
-
-    "Frogs have a 360° visual range! The eyes of a frog are made and positioned in such a way that at any given moment they can see everything around them, including what is behind them. As if this did not make them difficult enough to sneak up on, frogs also sleep with both of their eyes open."
-];
+const random = [animal,games,cars,prog];
 
 // Fuction to run when event listener is triggered
 const generateRandomMessage = (array) =>{
-    return array[Math.floor(Math.random() * array.length)]
+    let ranNum;
+    ranNum = Math.floor(Math.random() * array.length)
+    if(array === random){
+        let i = Math.floor(Math.random() * 5)
+        // Maybe add a more accurate randomiser that 5 above in the event we change the ammount of rendom messages per array
+        return array[ranNum][i]
+    }else{
+        return array[ranNum]
+    }
 }
-
-
-
-
-
 // Event Listeners for click on buttons
-animalsButton.addEventListener("click", function(){
-    message.textContent = generateRandomMessage(animal);
-});
-
-gamesButton.addEventListener('click', function(){
-    message.textContent = generateRandomMessage(games);
-});
-
-carsButton.addEventListener('click', function(){
-    message.textContent = generateRandomMessage(cars);
-});
-
-programmingButton.addEventListener('click', function(){
-    message.textContent = generateRandomMessage(prog);
-});
-
-randomButton.addEventListener('click', function(){
-    message.textContent = generateRandomMessage(random);
-});
+function click_event(button, array){
+    button.addEventListener("click", function(){
+        message.textContent = generateRandomMessage(array);
+    });
+}
+click_event(animalsButton,animal)
+click_event(gamesButton,games)
+click_event(carsButton,cars)
+click_event(programmingButton,prog)
+click_event(randomButton,random);
 
